4d3357fc9ce920f3d3ff8a56bea1b51355106c73
DangTrieeu <dangtrieu.lb@gmail.com>
2025-03-27 23:19:18 +0700

    catch exception

---
+++ src/main/java/vn/hoidanit/jobhunter/utils/FormatRestresponse.java
@@ -0,0 +1,43 @@
+package vn.hoidanit.jobhunter.utils;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import jakarta.servlet.http.HttpServletResponse;
+import vn.hoidanit.jobhunter.domain.RestResponse;
+
+@ControllerAdvice
+public class FormatRestresponse implements ResponseBodyAdvice {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return true;
+    }
+
+    @Override
+    @Nullable
+    public Object beforeBodyWrite(@Nullable Object body, MethodParameter returnType, MediaType selectedContentType,
+            Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+
+        HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
+        int status = servletResponse.getStatus();
+
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(status);
+        if (status >= 400) {
+            // case error
+            return body;
+        } else {
+            res.setMessage("CALL API FAILED");
+            res.setData(body);
+        }
+        return res;
+    }
+
+}
