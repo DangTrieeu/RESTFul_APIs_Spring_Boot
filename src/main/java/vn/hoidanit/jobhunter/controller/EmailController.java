43d02d07b144de1de0e24000b83854aa7b4b6c1b
DangTrieeu <dangtrieu.lb@gmail.com>
2025-06-07 22:36:42 +0700

    #129 cron job

--- src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
@@ -1,10 +1,12 @@
 package vn.hoidanit.jobhunter.controller;
 
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.transaction.Transactional;
 import vn.hoidanit.jobhunter.service.EmailService;
 import vn.hoidanit.jobhunter.service.SubscriberService;
 import vn.hoidanit.jobhunter.util.annotation.ApiMessage;
@@ -23,6 +25,8 @@ public class EmailController {
 
     @GetMapping("/email")
     @ApiMessage("Send email")
+    @Scheduled(cron = "0/30 * * * * ?")
+    @Transactional
     public String sendEmail() {
         // emailService.sendEmail();
         // this.emailService.sendEmailSync("dangtrieu.lb@gmail.com",
@@ -30,6 +34,7 @@ public class EmailController {
         // "<h1>Hello World! This is a test email from Spring Boot application.</h1>",
         // false,
         // true);
+        System.out.println("Send email");
         this.subscriberService.sendSubscribersEmailJobs();
         return "ok";
     }
