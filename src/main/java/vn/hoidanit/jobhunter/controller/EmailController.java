ae66c46d851d16f9126ccac22534029d886504c2
DangTrieeu <dangtrieu.lb@gmail.com>
2025-06-07 22:19:03 +0700

    #127 complete send email function

--- src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
@@ -6,15 +6,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.jobhunter.service.EmailService;
+import vn.hoidanit.jobhunter.service.SubscriberService;
 import vn.hoidanit.jobhunter.util.annotation.ApiMessage;
 
 @RestController
 @RequestMapping("/api/v1")
 public class EmailController {
     private final EmailService emailService;
+    private final SubscriberService subscriberService;
 
-    public EmailController(EmailService emailService) {
+    public EmailController(EmailService emailService, SubscriberService subscriberService) {
         this.emailService = emailService;
+        this.subscriberService = subscriberService;
 
     }
 
@@ -27,9 +30,7 @@ public class EmailController {
         // "<h1>Hello World! This is a test email from Spring Boot application.</h1>",
         // false,
         // true);
-        this.emailService.sendEmailFromTemplateSync("dangtrieu.lb@gmail.com",
-                "Test from Spring Boot",
-                "job");
+        this.subscriberService.sendSubscribersEmailJobs();
         return "ok";
     }
 }
--- src/main/java/vn/hoidanit/jobhunter/service/EmailService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/EmailService.java
@@ -1,47 +1,54 @@
 package vn.hoidanit.jobhunter.service;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import vn.hoidanit.jobhunter.domain.Job;
+import vn.hoidanit.jobhunter.repository.JobRepository;
 
 @Service
 public class EmailService {
+
     private final MailSender mailSender;
     private final JavaMailSender javaMailSender;
     private final SpringTemplateEngine templateEngine;
+    private final JobRepository jobRepository;
 
-    public EmailService(MailSender mailSender, JavaMailSender javaMailSender,
-            SpringTemplateEngine templateEngine) {
+    public EmailService(MailSender mailSender,
+            JavaMailSender javaMailSender,
+            SpringTemplateEngine templateEngine,
+            JobRepository jobRepository) {
         this.mailSender = mailSender;
         this.javaMailSender = javaMailSender;
         this.templateEngine = templateEngine;
+        this.jobRepository = jobRepository;
     }
 
-    public void sendEmail() {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo("dangtrieu.lb@gmail.com");
-        message.setSubject("Test from Spring Boot");
-        message.setText("Hello World! This is a test email from Spring Boot application.");
-        this.mailSender.send(message);
+    public void sendSimpleEmail() {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("ads.hoidanit@gmail.com");
+        msg.setSubject("Testing from Spring Boot");
+        msg.setText("Hello World from Spring Boot Email");
+        this.mailSender.send(msg);
     }
 
-    public void sendEmailSync(String to, String subject, String content, boolean isMultipart,
-            boolean isHtml) {
+    public void sendEmailSync(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
         // Prepare message using a Spring helper
         MimeMessage mimeMessage = this.javaMailSender.createMimeMessage();
         try {
-            MimeMessageHelper message = new MimeMessageHelper(mimeMessage,
-                    isMultipart, StandardCharsets.UTF_8.name());
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, isMultipart, StandardCharsets.UTF_8.name());
             message.setTo(to);
             message.setSubject(subject);
             message.setText(content, isHtml);
@@ -51,9 +58,19 @@ public class EmailService {
         }
     }
 
-    public void sendEmailFromTemplateSync(String to, String subject, String templateName) {
+    @Async
+    public void sendEmailFromTemplateSync(
+            String to,
+            String subject,
+            String templateName,
+            String username,
+            Object value) {
+
         Context context = new Context();
-        String content = this.templateEngine.process(templateName, context);
+        context.setVariable("name", username);
+        context.setVariable("jobs", value);
+
+        String content = templateEngine.process(templateName, context);
         this.sendEmailSync(to, subject, content, false, true);
     }
 
